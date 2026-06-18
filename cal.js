209ab5aa3e9e2839976a27e2465f07a6cff80ad4
Asif9652 <rayadurgamasif@gmail.com>
2026-06-18 19:38:28 +0530

    added keyboard version

--- cal.js
+++ cal.js
@@ -16,3 +16,37 @@ function gobackk(){
 
     display.value=l.slice(0,-1);
 }
+
+
+document.addEventListener("keydown", (e)=>{
+    if("1234567890%^*().-/+".includes(e.key)){
+    display.value=display.value+e.key;}
+       if(e.key === "Enter"){
+        try{
+            display.value = eval(display.value);
+        }
+        catch{
+            display.value = "Error";
+        }
+    }
+
+      if(e.key === "Backspace"){
+        display.value = display.value.slice(0,-1);
+    }
+
+    if(e.key === "Escape"){
+        display.value = "";
+    }
+});
+
+document.addEventListener("keydown",(e)=>{
+    let btn = document.getElementById("btn"+e.key);
+    if(btn){
+        btn.classList.add("active-key");
+
+        setTimeout(()=>{
+            btn.classList.remove("active-key");
+        },100);
+    }
+
+});
